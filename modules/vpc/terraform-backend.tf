05d228a851191676cc818c24917a6cafed510837
kemgou <vidalngka@gmail.com>
2024-06-29 05:20:20 -0500

    moved files into resources

--- modules/vpc/terraform-backend.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "ektec-terraform-state"
-#     key            = "aws-terraform/vpc/vpc-01/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "ektec-terraform-state-lock"
-#   }
-# }
