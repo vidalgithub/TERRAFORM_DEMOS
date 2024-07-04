10e9ee9cff72baae63925427fda228059afb339d
vidalgithub <88409846+vidalgithub@users.noreply.github.com>
2024-07-04 06:03:04 -0500

    Add files via upload

---
+++ resources/09-eksDevelopers-with-R-RB-iamrole-iamgroup-copy/c3-01-generic-variables.tf
@@ -0,0 +1,35 @@
+# Input Variables - Placeholder file
+# AWS Region
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type = string
+  default = "us-east-1"  
+}
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type = string
+  default = "dev"
+}
+# Business Division
+variable "business_divsion" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type = string
+  default = "SAP"
+}
+
+# EKS Cluster Input Variables
+variable "cluster_name" {
+  description = "Name of the EKS cluster. Also used as a prefix in names of related resources."
+  type        = string
+  default     = "eksdemo"
+}
+
+# Role Input Variables
+variable "role_namespace" {
+  description = "Name of the namespace associated to the role."
+  type        = string
+  default     = "dev"
+}
+
+
---
+++ resources/09-eksDevelopers-with-R-RB-iamrole-iamgroup-copy/terraform.tfvars
@@ -0,0 +1,12 @@
+# Generic Variables
+aws_region = "us-east-1"
+environment = "dev"
+business_divsion = "hr"
+cluster_name    = "eksdemo1"
+role_namespace = "dev"
+
+
+
+
+
+
