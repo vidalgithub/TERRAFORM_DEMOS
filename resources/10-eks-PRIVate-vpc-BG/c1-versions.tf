bb387db16d3bf98b81e02b99eced8990e9c9a71d
vidalgithub <88409846+vidalgithub@users.noreply.github.com>
2024-08-07 19:56:24 -0500

    Update c1-versions.tf

--- resources/10-eks-PRIVate-vpc-BG/c1-versions.tf
+++ resources/10-eks-PRIVate-vpc-BG/c1-versions.tf
@@ -19,7 +19,32 @@ terraform {
   }
 }
 
+############# CONNECT AND GENERATE TEMPORARY AWS CREDENTIALS ################
+# provider "vault" {
+#     address = "http://vault.beitcloud.com:8200"
+# }
+
+provider "vault" {}
+# First, set Vault server and token as env variables - see script.sh
+
+variable "cred_backend" {
+  default = "aws-admin-backend"
+}
+variable "cred_role_name" {
+  default = "aws-admin-role"
+}
+
+# Generate Dynamic AWS credentials
+data "vault_aws_access_credentials" "creds" {
+  backend = var.cred_backend  # vault_aws_secret_backend.aws.path
+  role    = var.cred_role_name  #vault_aws_secret_backend_role.role.name
+}
+
 # Terraform Provider Block
+# Use the generated Dynamic AWS credentials (from data block) connect to AWS backend platform and provision/create your infrastruture
 provider "aws" {
   region = var.aws_region
+  access_key = data.vault_aws_access_credentials.creds.access_key
+  secret_key = data.vault_aws_access_credentials.creds.secret_key
 }
+
